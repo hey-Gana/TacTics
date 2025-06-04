ce7bf3a4e54e7253752926e194127c1e66305aea
hey-Gana <ganasubs98@gmail.com>
2025-06-04 12:11:21 -0500

    Initial commit of Gobblets Gobblers Flutter app

---
+++ README.md
@@ -0,0 +1,3 @@
+# gobblets_gobblers_game
+A modern twist on Tic-Tac-Toe with stacking!
+
---
+++ android/app/src/main/kotlin/com/example/gobblets_gobblers_game/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.gobblets_gobblers_game
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
